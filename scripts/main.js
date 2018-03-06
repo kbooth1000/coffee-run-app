5c402c390e87358aea8c009f3e8cda578ce61478
Kyle Booth <kbooth1000@gmail.com>
2018-03-06 12:55:36 -0500

    Change row color before deleting

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,8 @@ var getRetrievedOrders = fetch(url)
     .then(function(response) {
         return response.json();
     });
-var postOrder = function(order){
-    console.log('ORDER: '+ order);
-    
+
+var postOrder = function(order){    
     var postRequest = fetch(url, {
         method: 'POST',
         body: JSON.stringify(order),
@@ -19,15 +18,33 @@ var postOrder = function(order){
         })
     });
 }
-var deleteOrder = function(order) {
-    console.log('Delete: ' + order.emailAddress);
-    fetch(url + '/' + order.emailAddress, {
+
+
+
+var deleteOrder = function(e, order) {
+    
+    var deleteRequest = function(){
+        fetch(url + '/' + order.emailAddress, {
         method: 'DELETE'
-    }).then(response =>
-        response.json().then(json => {
-            return json;
-        })
-    );
+        }).then(response =>
+            response.json().then(json => {
+                return json;
+            })
+        ).then(function(){
+            e.target.remove();
+        });
+    }
+    var flashRow = function(el) {
+        var colorInterval;
+        el.classList.add('selected');
+       // colorInterval = 
+       window.setInterval(function() {
+            el.classList.remove('selected');
+            deleteRequest();
+        }, 1100);
+        clearInterval(colorInterval);
+    }
+    flashRow(e.target);
 }
 
 var orderForm = document.querySelector('form');
@@ -44,18 +61,10 @@ var getOrderSize = function() { // new value after user selects radio button
     }
 }
 
-
-
 var orderSizes = orderForm.querySelectorAll('[name="size"]:checked');
 var orderFlavor = orderForm.querySelector('[name="flavor"]');
 var orderCaffeine = orderForm.querySelector('[name="strength"]');
 var orderNumber = 0;
-
-var postNewOrder = function(order) {
-    postOrder(order);
-    console.log('###'+ order);
-    
-}
 
 var compileOrder = function(e) {
     e.preventDefault();
@@ -68,12 +77,11 @@ var compileOrder = function(e) {
     for (var i = 0; i < orderInputs.length; i++) {
         coffeeOrders[orderNumber][i] = orderInputs[i].value;
         keyPairs.push( [keyNames[i], orderInputs[i].value] );
-        console.log('keyPairs: '+keyPairs[i]);
     }
     var keyPairObj = keyPairs.reduce(function(prev,curr){prev[curr[0]]=curr[1];return prev;},{});
     console.log('keyPairObj: ' + keyPairObj);
     
-    postNewOrder(keyPairObj);
+    postOrder(keyPairObj);
     createOrderDisplay(orderNumber);
     createDisplayRow(keyPairObj);
     orderNumber++;
@@ -84,7 +92,6 @@ var orderDisplay = document.querySelector('.orderDisplay');
 var orderList = document.createElement('ul');
 orderList.textContent = '';
 orderDisplay.appendChild(orderList);
-
 
 var createOrderDisplay = function(orderNumber) {
     var orderDisplayString = '';
@@ -139,7 +146,6 @@ var getOrderData = function(data){
              orderObject['coffee']]);
          }
      }
-     console.log('retrievedOrdersList: '+retrievedOrdersList);
          return retrievedOrdersList;
  }
 
@@ -147,13 +153,11 @@ var createDisplayRow = function(order) {
             var orderRow = document.createElement('li'); // Create display row
             orderRow.className = 'orderRow';
             orderList.appendChild(orderRow);
-            orderRow.textContent += `${order.coffee}: ${order.emailAddress}, ${order.size}`; 
-            orderRow.addEventListener('click', function() {
-                deleteOrder(order)
+            orderRow.textContent += `${order.coffee}: ${order.emailAddress}, ${order.size}, ${order.flavor}, ${order.strength}`; 
+            orderRow.addEventListener('click', function(e) {
+                deleteOrder(e, order)
             });
 };
-
-
 
 var orderObject = {};
 
@@ -161,50 +165,12 @@ var test = function(data) {
     console.log('testdata: '+ data);
 }
 
-
-
 getRetrievedOrders.then(function(data) {
         keys = [];
         for(key in data) {
             keys.push(data[key]);          
         }
-      //  console.log('keys: '+keys);
         getOrderData(keys);
     });
 
 createOrderDisplay();
-
-  
-  
-// orderID, orderCaffeine, orderFlavor, orderSize, orderEmail, orderName
-
-
-
-
-
-
-
-// NICK'S CODE
-// orders = [                                                                         
-//     {"strength":0,"flavor":"None","size":"tall","emailAddress":"person@email.com","coffee":"Coffee"}, 
-//     {"strength":3,"flavor":"mocha","size":"tall","emailAddress":"jamlicious@gmail.com","coffee":"latte"},
-//     {"flavor":"almond","strength":30,"size":"short","emailAddress":"xyz@who.com","coffee":"latte"},
-//     {"strength":10,"flavor":"caramel","size":"tall","emailAddress":"uni@gallop.org","coffee":"love"},
-// ]                                                                                  
-                                                                                   
-// var $popBtn = $('#popBtn');                                                        
-// $popBtn[0].addEventListener('click', function(event) {                             
-//     event.preventDefault();                                                        
-//     for (var i = 0; i < orders.length; i++) {                                      
-//         var order = {                                                              
-//             coffee: orders[i]['coffee'],                                           
-//             emailAddress: orders[i]['emailAddress'],                               
-//             size: orders[i]['size'],                                               
-//             flavor: orders[i]['flavor'],                                           
-//             strength: orders[i]['strength']                                        
-//         };                                                                         
-//         $.post("https://dc-coffeerun.herokuapp.com/api/coffeeorders", order, function(resp) {
-//             console.log(resp)                                                      
-//         });                                                                        
-//     }                                                                              
-// });              
